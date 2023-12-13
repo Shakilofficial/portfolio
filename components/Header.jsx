560e29fecc12c4fcf2c684ebb771fa4f1a0f8289
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-12-13 22:04:45 +0600

    service page added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import Logo from "./Logo";
 import MobileNavbar from "./MobileNavbar";
 import Navbar from "./Navbar";
 import ThemeToggler from "./ThemeToggler";
-import { usePathname } from "next/navigation";
 
 const Header = () => {
   const [header, setHeader] = useState(false);
@@ -30,12 +30,12 @@ const Header = () => {
           <Logo />
           <div className="flex  items-center gap-x-6">
             <Navbar
-              containerStyles="hidden xl:flex gap-x-8 items-center"
+              containerStyles="hidden lg:flex gap-x-8 items-center"
               linkStyles="relative hover:text-primary transition-all"
               underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
             />
             <ThemeToggler />
-            <div className="xl:hidden ">
+            <div className="lg:hidden ">
               <MobileNavbar />
             </div>
           </div>
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,19 +1,63 @@
-import {GanttChartSquare, Blocks,Gem } from "lucide-react"; 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { GanttChartSquare, Blocks, Gem } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 const ServicesData = [
-    {
-        icon: <GanttChartSquare size={72} StrokeWidth={0.8} />,
-        title: 'Web Design',
-        description:''
-    }
-]
+  {
+    icon: <GanttChartSquare size={72} StrokeWidth={0.8} />,
+    title: "Web Design",
+    description:
+      "Crafting user-friendly and visually stunning web interfaces to leave a lasting impression on your audience.",
+  },
+  {
+    icon: <Blocks size={72} StrokeWidth={0.8} />,
+    title: "Web Development",
+    description:
+      "Building robust and scalable web applications using cutting-edge technologies for a seamless digital experience.",
+  },
+  {
+    icon: <Gem size={72} StrokeWidth={0.8} />,
+    title: "Figma to Web App",
+    description:
+      "Transforming Figma designs into captivating and responsive web applications, bringing your creative vision to life online.",
+  },
+];
+
 const Services = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="mb-12 lg:mb-36">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-12 lg:mb-24 text-center mx-auto">
+          My Services
+        </h2>
+        <div className="grid lg:grid-cols-3 justify-center gap-y-12 lg:gap-y-24 lg:gap-x-8">
+          {ServicesData.map((item, index) => {
+            return (
+              <Card
+                className="w-full max-w-[400px] h-[300px] flex flex-col pt-16 pb-10 justify-center items-center relative"
+                key={index}
+              >
+                <CardHeader className="text-primary absolute -top-[60px]">
+                  <div className="w-[140px] h-[80px] bg-white dark:bg-background flex justify-center items-center">
+                    {item.icon}
+                  </div>
+                </CardHeader>
+                <CardContent className="text-center space-y-3">
+                  <CardTitle>{item.title} </CardTitle>
+                  <CardDescription>{item.description}</CardDescription>
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Services;
